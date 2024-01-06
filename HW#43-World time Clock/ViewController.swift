9fb61e3046d99789a1596d393f5961b9912015f3
Daweihao84 <dwsamurai84@gmail.com>
2024-01-06 14:36:41 +0800

    Initial Commit

---
+++ HW#43-World time Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW#43-World time Clock
+//
+//  Created by Da-wei Hao on 2024/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW#43-World time ClockUITests/HW_43_World_time_ClockUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HW_43_World_time_ClockUITestsLaunchTests.swift
+//  HW#43-World time ClockUITests
+//
+//  Created by Da-wei Hao on 2024/1/6.
+//
+
+import XCTest
+
+final class HW_43_World_time_ClockUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
